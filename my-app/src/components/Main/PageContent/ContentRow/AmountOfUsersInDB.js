d48e3fec670f477a43f0c5743227af7ad185e40c
Nicolas Belelli <nbelelli@gmail.com>
2021-02-28 00:41:44 -0300

    shows quantity of users in database in the dashboard

--- my-app/src/components/Main/PageContent/ContentRow/AmountOfUsersInDB.js
+++ my-app/src/components/Main/PageContent/ContentRow/AmountOfUsersInDB.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function AmountOfUsersInDB() {
+	const [quantityUsers, setQuantityUsers] = useState(0);
+
+	useEffect(() => {
+		fetch('http://localhost:3000/api/users')
+			.then((res) => res.json())
+			.then((data) => setQuantityUsers(data.meta.count));
+	}, []);
 	return (
 		<div className="col-md-4 mb-4">
 			<div className="card border-left-warning shadow h-100 py-2">
@@ -11,7 +18,9 @@ function AmountOfUsersInDB() {
 							<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
 								Users quantity
 							</div>
-							<div className="h5 mb-0 font-weight-bold text-gray-800">38</div>
+							<div className="h5 mb-0 font-weight-bold text-gray-800">
+								{quantityUsers}
+							</div>
 						</div>
 						<div className="col-auto">
 							<i className="fas fa-user-check fa-2x text-gray-300"></i>
